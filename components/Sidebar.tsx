8a87142d62aa828a82235aef57c1eeb6b957e60f
Coderrexe <shizimo@gmail.com>
2025-06-27 18:41:43 +0100

    set up collectionGroup and start working on sidebar logic

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,5 +1,9 @@
+"use client"
+
 import { MenuIcon } from "lucide-react";
 import NewDocumentButton from "./NewDocumentButton";
+import { useCollection } from "react-firebase-hooks/firestore";
+
 import {
   Sheet,
   SheetContent,
@@ -7,8 +11,32 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { useUser } from "@clerk/nextjs";
+import { collectionGroup, DocumentData, query, where } from "firebase/firestore";
+import { db } from "@/firebase";
+import { useEffect } from "react";
+
+interface RoomDocument extends DocumentData {
+  createdAt: Date;
+  role: "owner" | "editor";
+  roomId: string;
+  userId: string;
+}
 
 function Sidebar() {
+  const { user } = useUser();
+
+  const [data, loading, error] = useCollection(
+    user &&
+      query(
+        // collectionGroup -> give me all documents in all rooms subcollections,
+        // anywhere in the DB - requires setting up index exemptions for userId
+        // and roomId of rooms subcollection in Firestore console.
+        collectionGroup(db, "rooms"),
+        where("userId", "==", user.emailAddresses[0].toString())
+      )
+  );
+
   const menuOptions = (
     <>
       <NewDocumentButton />
