c912e537bd03d956de9160dc79f00ae9c4276d4b
Coderrexe <shizimo@gmail.com>
2025-06-28 19:13:29 +0100

    start document page

---
+++ components/Document.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+function Document({ id }: { id: string }) {
+  return (
+    <div>
+      <div>
+        <form action=""></form>
+      </div>
+      <div>
+        {/* ManageUsers */}
+
+        {/* Avatars */}
+      </div>
+
+      {/* Collaborative editor */}
+    </div>
+  );
+}
+
+export default Document;
