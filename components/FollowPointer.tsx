83d4e07bd1ed83e04f10c0d87e7f3752bd2354f8
Coderrexe <shizimo@gmail.com>
2025-07-08 20:59:28 +0100

    fix followpointer

--- components/FollowPointer.tsx
+++ components/FollowPointer.tsx
@@ -34,7 +34,7 @@ function FollowPointer({
         width="1em"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <path d="M14.082 2.182a.5.5 0 0 1 .103.557l8.528 15.467a.5.5 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103z"></path>
+        <path d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103z"></path>
       </svg>
       <motion.div
         className="px-2 py-2 bg-neutral-200 text-black font-bold whitespace-nowrap min-w-max text-xs rounded-full"
@@ -43,7 +43,7 @@ function FollowPointer({
         animate={{ scale: 1, opacity: 1 }}
         exit={{ scale: 0.5, opacity: 0 }}
       >
-        {info?.email || info.email}
+        {info?.name || info.email}
       </motion.div>
     </motion.div>
   );
